a4773cab7607e22f51ef4681fb6839195ccad26e
Hari <imamhari3001@gmail.com>
2025-02-27 22:52:00 +0700

    carousel ok

--- src/app/component/Carousel.jsx
+++ src/app/component/Carousel.jsx
@@ -1,6 +1,10 @@
 "use client";
-import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay, Pagination } from "swiper/modules";
 import { ChevronRight, ChevronLeft } from "lucide-react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const items = [
   {
@@ -33,61 +37,87 @@ const items = [
     count: 11,
     bg: "bg-[#F3F7D9]",
   },
+  {
+    name: "Cupcake",
+    icon: "/assets/icon/cupcake.png",
+    count: 22,
+    bg: "bg-[#F0FEEB]",
+  },
+  {
+    name: "Pizza",
+    icon: "/assets/icon/pizza.png",
+    count: 25,
+    bg: "bg-[#E4F2F4]",
+  },
+  {
+    name: "Kebab",
+    icon: "/assets/icon/kebab.png",
+    count: 12,
+    bg: "bg-[#EAEEFA]",
+  },
+  {
+    name: "Salmon",
+    icon: "/assets/icon/salmon.png",
+    count: 22,
+    bg: "bg-[#F9EEF3]",
+  },
+  {
+    name: "Doughnut",
+    icon: "/assets/icon/doughnut.png",
+    count: 11,
+    bg: "bg-[#F3F7D9]",
+  },
+  
 ];
 
 function Carousel() {
-  const [current, setCurrent] = useState(0);
-
-  const nextSlide = () => {
-    setCurrent((prev) => (prev === items.length - 1 ? 0 : prev + 1));
-  };
-
-  const prevSlide = () => {
-    setCurrent((prev) => (prev === 0 ? items.length - 1 : prev - 1));
-  };
-
   return (
     <section className="flex flex-col">
-      <div className="flex justify-center overflow-hidden max-w-screen xl xl:max-w-screen-2xl  w-[1333px] h-[215px] p-[10px] gap-5">
+      <div className="w-[1333px] h-[215px]">
+      <Swiper
+        modules={[Navigation, Autoplay]}
+        spaceBetween={7}
+        slidesPerView={5}
+        navigation={{
+          nextEl: ".next-btn",
+          prevEl: ".prev-btn",
+        }}
+        
+        autoplay={{ delay: 3000 }}
+        loop={true}
+        className="flex justify-center max-w-screen xl:max-w-screen-2xl h-[200px]  items-center"
+      >
         {items.map((item, index) => (
-          <div
-            key={index}
-            className={`relative flex flex-col items-center justify-center w-[230px] h-[175px] p-4 rounded-xl shadow-lg transition-all duration-300 cursor-pointer
-              ${index === current ? "scale-110 shadow-xl border-2 border-gray-300"  : "scale-100"}
-              ${item.bg}
-              hover:scale-110`}
-          >
+          <SwiperSlide key={index} className="flex justify-center items-center pt-4">
             <div
-              className={`absolute inset-0 bg-cover bg-center transition-opacity duration-300 
-                ${index === current ? "opacity-20" : "opacity-0"}
-                hover:opacity-20`}
-              style={{ backgroundImage: "url('/assets/backgroud.png')" }}
-            ></div>
-            {/* Icon */}
-            <img
-              src={item.icon}
-              alt={item.name}
-              className="w-16 h-16 relative z-10"
-            />
-            <h4 className="relative z-10 text-lg font-semibold">{item.name}</h4>
-            <p className="relative z-10 text-sm">{item.count} Items</p>
-          </div>
-        ))}
-      </div>
+              className={`${item.bg} relative flex flex-col items-center justify-center w-[230px] h-[175px]  rounded-xl shadow-lg transition-all duration-300 cursor-pointer hover:scale-110 hover:z-20`}
+            >
+              <div
+                className="absolute inset-0 bg-cover bg-center opacity-0 hover:opacity-20 transition-opacity"
+                style={{ backgroundImage: "url('/assets/backgroud.png')" }}
+              ></div>
 
-      {/* Navigation Buttons */}
-      <div className="flex justify-end mr-20 gap-4">
-        <button
-          onClick={prevSlide}
-          className="flex justify-center items-center gap-2 w-[113px] h-[50px] bg-[#8BAC3E] text-white rounded-full shadow-md hover:bg-[#A4B441] transition"
-        >
+              <img
+                src={item.icon}
+                alt={item.name}
+                className="w-16 h-16 relative z-10"
+              />
+              <h4 className="relative z-10 text-lg font-semibold">
+                {item.name}
+              </h4>
+              <p className="relative z-10 text-sm">{item.count}</p>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      </div>
+      
+      <div className="xl:flex justify-end mr-20 gap-4 mt-4 hidden">
+        <button className="prev-btn flex justify-center items-center gap-2 w-[113px] h-[50px] bg-[#8BAC3E] text-white rounded-full shadow-md hover:bg-[#A4B441] transition">
           <ChevronLeft size={20} className="bg-white text-[#8BAC3E] rounded-full w-[33px] h-[33px]" />
           PREV
         </button>
-        <button
-          onClick={nextSlide}
-          className="flex justify-center items-center gap-2 px-4 py-2 bg-[#8BAC3E] hover:bg-[#A4B441] text-white rounded-full shadow-md  transition w-[113px] h-[50px]"
-        >
+        <button className="next-btn flex justify-center items-center gap-2 px-4 py-2 bg-[#8BAC3E] hover:bg-[#A4B441] text-white rounded-full shadow-md transition w-[113px] h-[50px]">
           NEXT
           <ChevronRight size={20} className="bg-white text-[#8BAC3E] rounded-full w-[33px] h-[33px]" />
         </button>
